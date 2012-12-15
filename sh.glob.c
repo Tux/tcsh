bda6ca362086880d8f3333d5966fd72b6c00541a
christos <christos>
2012-12-15 23:20:25 +0000

    Revert 3.79; breaks: cat << _EOF > huh #!/bin/sh echo "[$@]" echo "I am running" echo "I am running ($$)" >> huh.out _EOF cat << _EOF > huh.tcsh #!/bin/tcsh -f
    ./huh \
        `#comment blah blah blah` \
        parameter a \
        `#comment blah blah blah` \
        parameter b \
        `#comment blah blah blah` \
        parameter c
    echo ok
    _EOF

--- sh.glob.c
+++ sh.glob.c
@@ -781,8 +781,6 @@ backeval(struct blk_buf *bb, struct Strbuf *word, Char *cp, int literal)
 		stderror(ERR_OLD);
 	    alias(&paraml);
 	    t = syntax(paraml.next, &paraml, 0);
-	    if (t == NULL)
-		return;
 	    cleanup_push(t, syntax_cleanup);
 	    /* The F_BACKQ flag must set so the job output is correct if
 	     * printexitvalue is set.  If it's not set, the job output
