8b6a8354f290e62891970f1a27903db58764cc02
christos <christos>
2014-07-18 19:45:35 +0000

    prepare for 6.18.02

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 18
-#define PATCHLEVEL 1
-#define DATE "2012-02-14"
+#define PATCHLEVEL 2
+#define DATE "2014-07-18"
 
 #endif /* _h_patchlevel */
