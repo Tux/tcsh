5f6bd0fbca9837f47175e1488b801d71518cb057
christos <christos>
2015-05-10 13:32:44 +0000

    welcome to 05

--- patchlevel.h
+++ patchlevel.h
@@ -8,7 +8,7 @@
 #define ORIGIN "Astron"
 #define REV 6
 #define VERS 18
-#define PATCHLEVEL 4
-#define DATE "2015-05-04"
+#define PATCHLEVEL 5
+#define DATE "2015-05-10"
 
 #endif /* _h_patchlevel */
